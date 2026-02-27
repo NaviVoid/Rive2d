15b754ede296d903b7f41bc34e190e1668dd25a7
NaviVoid <549425867@qq.com>
2026-02-28 01:10:48 +0800

    use untitled-pixi-live2d-engine

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js';
-import { Live2DModel } from '@jannchie/pixi-live2d-display';
+import { Live2DModel } from 'untitled-pixi-live2d-engine';
 
 // Expose PIXI globally for pixi-live2d-display
 window.PIXI = PIXI;
@@ -27,7 +27,6 @@ const ready = app.init({
   resizeTo: window,
   antialias: true,
   preference: 'webgl',   // WebKitGTK has no WebGPU
-  useBackBuffer: true,   // Force full-buffer blit to fix Wayland transparent surface smearing
 }).then(() => {
   app.stage.addChild(borderGfx);
 
